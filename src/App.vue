44126405c9acde5f6a8845dc4e4c45012ff59226
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-31 15:32:55 +0700

    feat: Add link to original version

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,9 @@
       Notice: Please use Google Chrome on Desktop or Android only. Other
       browsers are not supported.
     </div>
+    <div class="p-2 text-sm bg-gray-100 underline">
+      <a href="https://misheard.vercel.app">See Original Version</a>
+    </div>
     <div class="p-4 bg-white">
       <template v-if="!langIsSet">
         <h2 class="text-3xl mb-6">Select language</h2>
@@ -61,8 +64,7 @@
             class="
               ml-2
               border border-red-500
-              hover:bg-red-700
-              hover:text-white
+              hover:bg-red-700 hover:text-white
               py-2
               px-4
               rounded
@@ -77,8 +79,7 @@
           <button
             class="
               border border-blue-500
-              hover:bg-blue-700
-              hover:text-white
+              hover:bg-blue-700 hover:text-white
               py-2
               px-4
               rounded
@@ -109,82 +110,82 @@
 </template>
 
 <script lang="ts">
-import { computed, defineComponent, onUpdated, reactive, ref } from "vue";
-import { sharedMessages } from "./doc";
-import { lang, langIsSet } from "./params";
-import { langs } from "./langs";
+import { computed, defineComponent, onUpdated, reactive, ref } from "vue"
+import { sharedMessages } from "./doc"
+import { lang, langIsSet } from "./params"
+import { langs } from "./langs"
 import Skoy from "skoy"
 
-declare var webkitSpeechRecognition: typeof SpeechRecognition;
+declare var webkitSpeechRecognition: typeof SpeechRecognition
 
 export default defineComponent({
   name: "App",
   setup() {
-    const name = ref(localStorage.MISHEARD_NAME ?? "");
-    const currentPrompt = ref("");
-    const messages = ref(sharedMessages.toJSON());
-    const listening = ref(false);
-    const draft = ref("");
-    const sr = new webkitSpeechRecognition();
-    sr.lang = lang;
-    const sr2 = new webkitSpeechRecognition();
-    sr2.lang = lang;
-    sr2.continuous = true;
-    sr2.interimResults = true;
+    const name = ref(localStorage.MISHEARD_NAME ?? "")
+    const currentPrompt = ref("")
+    const messages = ref(sharedMessages.toJSON())
+    const listening = ref(false)
+    const draft = ref("")
+    const sr = new webkitSpeechRecognition()
+    sr.lang = lang
+    const sr2 = new webkitSpeechRecognition()
+    sr2.lang = lang
+    sr2.continuous = true
+    sr2.interimResults = true
 
-    let lastMessageCreatedAt: string | undefined;
+    let lastMessageCreatedAt: string | undefined
     sharedMessages.observe(() => {
-      messages.value = sharedMessages.toJSON();
+      messages.value = sharedMessages.toJSON()
 
-      const lastMessage = messages.value[messages.value.length - 1];
+      const lastMessage = messages.value[messages.value.length - 1]
       if (lastMessage) {
         if (lastMessageCreatedAt !== lastMessage.createdAt) {
-          lastMessageCreatedAt = lastMessage.createdAt;
-          (window as any).handleNewMessage?.(lastMessage);
+          lastMessageCreatedAt = lastMessage.createdAt
+          ;(window as any).handleNewMessage?.(lastMessage)
         }
       }
-    });
+    })
 
     const ask = async (prompt: string): Promise<string> => {
       try {
-        currentPrompt.value = prompt;
+        currentPrompt.value = prompt
         return await new Promise((resolve, reject) => {
           sr.onresult = (event) => {
             const text = Array.from(event.results)
               .map((alts) => alts[0].transcript)
-              .join("");
-            resolve(text);
-          };
+              .join("")
+            resolve(text)
+          }
           sr.onerror = (e) => {
-            reject(new Error("Unable to recognize text: " + e.error));
-          };
-          sr.start();
-        });
+            reject(new Error("Unable to recognize text: " + e.error))
+          }
+          sr.start()
+        })
       } finally {
-        currentPrompt.value = "";
+        currentPrompt.value = ""
       }
-    };
+    }
     const login = async () => {
-      localStorage.MISHEARD_NAME = name.value = Skoy.convert(await ask(
-        "Say your name out loud"
-      ));
-    };
+      localStorage.MISHEARD_NAME = name.value = Skoy.convert(
+        await ask("Say your name out loud")
+      )
+    }
     const changeName = async () => {
-      localStorage.MISHEARD_NAME = name.value = "";
-    };
+      localStorage.MISHEARD_NAME = name.value = ""
+    }
     const send = async () => {
-      listening.value = true;
+      listening.value = true
       sr2.onresult = function (event) {
-        var finalTranscript = "";
-        var interimTranscript = "";
+        var finalTranscript = ""
+        var interimTranscript = ""
         for (var i = event.resultIndex; i < event.results.length; ++i) {
           if (event.results[i].isFinal) {
-            finalTranscript += event.results[i][0].transcript;
+            finalTranscript += event.results[i][0].transcript
           } else {
-            interimTranscript += event.results[i][0].transcript;
+            interimTranscript += event.results[i][0].transcript
           }
         }
-        draft.value = interimTranscript;
+        draft.value = interimTranscript
         if (finalTranscript) {
           sharedMessages.push([
             {
@@ -192,37 +193,37 @@ export default defineComponent({
               name: name.value,
               createdAt: new Date().toJSON(),
             },
-          ]);
+          ])
         }
-      };
+      }
       sr2.onerror = () => {
-        listening.value = false;
-      };
+        listening.value = false
+      }
       sr2.onend = () => {
-        listening.value = false;
-      };
-      sr2.start();
-    };
+        listening.value = false
+      }
+      sr2.start()
+    }
     const stop = () => {
-      sr2.stop();
-      listening.value = false;
-    };
-    let scrollIsAtBottom = true;
+      sr2.stop()
+      listening.value = false
+    }
+    let scrollIsAtBottom = true
     window.onscroll = () => {
       scrollIsAtBottom =
         window.scrollY >
-        document.documentElement.scrollHeight - window.innerHeight - 16;
-    };
+        document.documentElement.scrollHeight - window.innerHeight - 16
+    }
     onUpdated(() => {
       if (scrollIsAtBottom) {
-        window.scrollBy(0, 1000);
+        window.scrollBy(0, 1000)
       }
-    });
+    })
     const locales = computed(() => {
       return langs
         .flatMap(([l, ...lo]) => lo)
-        .sort((a, b) => (a[0] < b[0] ? -1 : 1));
-    });
+        .sort((a, b) => (a[0] < b[0] ? -1 : 1))
+    })
     return {
       name,
       login,
@@ -236,9 +237,9 @@ export default defineComponent({
       langIsSet,
       langs,
       locales,
-    };
+    }
   },
-});
+})
 </script>
 
 <style>
