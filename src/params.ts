3197077d28bcc09c1e01567b5d28894fb7b8c66d
Manassarn Manoonchai <manassarn@gmail.com>
2021-07-31 15:35:14 +0700

    chore: Change room name

--- src/params.ts
+++ src/params.ts
@@ -1,5 +1,5 @@
 const params = new URLSearchParams(location.search)
 
-export const roomId = params.get('room_id') || 'misheard-default'
-export const lang = params.get('lang') || 'en_US'
-export const langIsSet = !!params.get('lang')
+export const roomId = params.get("room_id") || "misheard-skoy-default"
+export const lang = params.get("lang") || "en_US"
+export const langIsSet = !!params.get("lang")
